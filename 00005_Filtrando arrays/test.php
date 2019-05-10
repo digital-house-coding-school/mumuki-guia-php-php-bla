01bb7c973f777c889694a632f55fc5c2726b4f77
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-05-10 14:16:26 +0000

    Mumuki Export on 2019-05-10 14:16:26 +0000

--- 00005_Filtrando arrays/test.php
+++ 00005_Filtrando arrays/test.php
@@ -13,19 +13,19 @@ public function testDescriptionResultado(): void {
     $correto1 = getResultado($array1);
     $resultado1 = $this->filtraArray($array1);
 
-    $this->assertTrue($correto1 == $resultado1, "O filtro não está funcionando muito bem!");
+    $this->assertTrue($correto1 == $resultado1, "O filtro1 não está funcionando muito bem!");
 
     $array2 = [2, 5, 98, 97, 99];
     $correto2 = getResultado($array2);
     $resultado2 = $this->filtraArray($array2);
 
-    $this->assertTrue($correto2 == $resultado2, "O filtro não está funcionando muito bem!");
+    $this->assertTrue($correto2 == $resultado2, "O filtro2 não está funcionando muito bem!");
 
     $array3 = [100, 200, 300, 400, 401, 403, 405];
     $correto3 = getResultado($array3);
     $resultado3 = $this->filtraArray($array3);
 
-    $this->assertTrue($correto3 == $resultado3, "O filtro não está funcionando muito bem!");    
+    $this->assertTrue($correto3 == $resultado3, "O filtro3 não está funcionando muito bem!");    
   }
 
   private /*...content...*/
