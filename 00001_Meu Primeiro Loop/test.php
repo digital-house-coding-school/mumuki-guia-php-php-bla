351c7a314c862e48a0d8167b40ea6726fad6ed64
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-05-15 20:17:00 +0000

    Mumuki Export on 2019-05-15 20:17:00 +0000

--- 00001_Meu Primeiro Loop/test.php
+++ 00001_Meu Primeiro Loop/test.php
@@ -3,19 +3,26 @@ public function testDescriptionExample(): void {
   
   $resultados = $this->loop();
   
-  $this->assertTrue(count($resultados) === 11, 'El array no tiene 11 elementos');
+  $this->assertTrue(count($resultados) === 11, 'O array não tem 11 elementos');
  
-  $numeros = $resultados[0] == 0 && $resultados[1] == 1 && $resultados[2] == 2 && $resultados[3] == 3 && $resultados[4] == 4 && $resultados[5] == 5 && $resultados[6] == 6 && $resultados[7] == 7 && $resultados[8] == 8 && $resultados[9] == 9 && $resultados[10] == 10;
-  
-  $this->assertTrue($numeros, "El array no tiene los numeros del 0 al 10");
-  
-  $cantFors = substr_count($sol, "for");
-  
-  $this->assertTrue($cantFors > 0, "Parecería que no escribiste ningún for...");
-  
-  $cant9 = substr_count($sol, "9");
-  
-  $this->assertTrue($cant9 == 0, "No deberías escribir los números a mano...");
-}
+  $numeros = true;
 
-private /*...content...*/
+    for ($i = 0; $i < 11; $i) {
+      if ($resultados[$i] != $i) {
+        $numeros = false;
+        break;
+      }
+    }
+
+    $this->assertTrue($numeros, "O array não tem números de 0 a 10");
+
+    $contadorFor = substr_count($solucao, "for");
+
+    $this->assertTrue($contadorFor > 0, "Parece que você não utilizou nenhum for :(, tente resolver utilizando pelo menos um for!");
+
+    $contador9 = substr_count($solucao, "9");
+
+    $this->assertTrue($cant9 == 0, "Não preencha seu array manualmente...");
+  }
+
+  private /*...content...*/
