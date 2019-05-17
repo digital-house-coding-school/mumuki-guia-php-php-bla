550044df5648a723289eaa0b1349c2f52495cf3b
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-05-17 01:25:15 +0000

    Mumuki Export on 2019-05-17 01:25:15 +0000

--- 00008_Somas associativas/test.php
+++ 00008_Somas associativas/test.php
@@ -7,12 +7,12 @@ public function testDescriptionResultado(): void {
     $this->assertTrue($resultado1 == 60, "A soma de 10, 20 e 30 deveria ser 60");
 
     $array2 = ["primeiro" => 15, "segundo" => 10, "terceiro" => 5];
-    $resultado2 = $this->bucles($array2);
+    $resultado2 = $this->somaAssoc($array2);
 
     $this->assertTrue($resultado2 == 30, "A soma de 15, 10 e 5 deveria ser 30");
 
     $array3 = ["primeiro" => 4, "segundo" => 8, "terceiro" => 15];
-    $resultado3 = $this->bucles($array3);
+    $resultado3 = $this->somaAssoc($array3);
 
     $this->assertTrue($resultado3 == 27, "A soma de 4, 8 e 15 deveria ser 27");
 
