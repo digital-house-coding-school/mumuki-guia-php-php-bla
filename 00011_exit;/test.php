2a92f3a11e5a28a63f70e98983f6c3633d94c486
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-05-21 21:10:20 +0000

    Mumuki Export on 2019-05-21 21:10:20 +0000

--- 00011_exit;/test.php
+++ 00011_exit;/test.php
@@ -1,3 +1,15 @@
-public function testDescriptionExample(): void {
-  $this->assertTrue(true);
-}
+public function testDescriptionResultado(): void {
+    $solucao = '/*...content...*/';
+
+    $this->assertTrue(substr_count($solucao, "var_dump") > 0, "Está faltando o var_dump()!!!");
+
+    $bug = [
+      "primeiro" => 'Oops! Você me descobriu!',
+      "segundo" => "Eu sou um array ;)"
+    ];
+
+    $this->debug($bug);
+    $this->assertTrue(false, "Parece que você não utilizou nenhum exit :(");
+  }
+
+  /*...content...*/
