35c66b26707a2b0986902a21527263424a80ab9a
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-05-21 17:28:59 +0000

    Mumuki Export on 2019-05-21 17:28:59 +0000

--- 00009_Alterando chaves e valores/test.php
+++ 00009_Alterando chaves e valores/test.php
@@ -1,3 +1,25 @@
-public function testDescriptionExample(): void {
-  $this->assertTrue(true);
-}
+public function testDescriptionResultado(): void {
+    $solucao = '/*...content...*/';
+
+    function trocaCerta($array) {
+      $resultado = [];
+
+      foreach ($array as $key => $value) {
+        $resultado[$value] = $key;
+      }
+
+      return $resultado;
+    }
+
+    $cantores = [
+      "Gilberto" => "Gil",
+      "Tim" => "Maia",
+      "Luiz" => "Gonzaga"
+    ];
+
+    $resultado = $this->trocaValores($cantores);
+
+    $this->assertTrue($resultado == trocaCerta($cantores), "Sua função não está funcionando como deveria :(");
+  }
+
+  private /*...content...*/
